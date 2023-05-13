66979eaf7431fea8406c137aeba17d17a9d71f46
piti2703 <piotr.trzebuniak42@gmail.com>
2023-05-13 17:18:44 +0200

    2.0 version

--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,14 +1,3 @@
-enum Complexity {
-  simple,
-  challenging,
-  hard,
-}
-
-enum Affordability {
-  affordable,
-  pricey,
-  luxurious,
-}
 
 class Meal {
   const Meal({
@@ -18,13 +7,6 @@ class Meal {
     required this.imageUrl,
     required this.ingredients,
     required this.steps,
-    required this.duration,
-    required this.complexity,
-    required this.affordability,
-    required this.isGlutenFree,
-    required this.isLactoseFree,
-    required this.isVegan,
-    required this.isVegetarian,
   });
 
   final String id;
@@ -33,11 +15,4 @@ class Meal {
   final String imageUrl;
   final List<String> ingredients;
   final List<String> steps;
-  final int duration;
-  final Complexity complexity;
-  final Affordability affordability;
-  final bool isGlutenFree;
-  final bool isLactoseFree;
-  final bool isVegan;
-  final bool isVegetarian;
 }
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,14 +1,70 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:meals__app/data/dummy_data.dart';
 import 'package:meals__app/widgets/category_grid_item.dart';
 import 'package:meals__app/screens/meals.dart';
 import 'package:meals__app/models/category.dart';
+import 'package:http/http.dart' as http;
+import 'package:meals__app/models/meal.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = dummyMeals
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+List<Meal> loadedItems = [];
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+  // @override
+  void _loadItems() async {
+    loadedItems = [];
+    final url = Uri.https(
+        'flutter-meals-2b4c2-default-rtdb.firebaseio.com', 'meals.json');
+
+    final response = await http.get(url);
+    final Map<String, dynamic> listData =
+        json.decode(response.body);
+
+        
+    for (final item in listData.entries) {
+
+      List<String> ingredients = [];
+      List<String> categories = [];
+      List<String> steps = [];
+
+      for (var i = 1; i <= item.value['ingredients'].length; i++) {
+        ingredients.add(item.value['ingredients']['ingredient$i']); 
+      }
+      for (var i = 1; i <= item.value['categories'].length; i++) {
+        categories.add(item.value['categories']['category$i']); 
+      }
+      for (var i = 1; i <= item.value['steps'].length; i++) {
+        steps.add(item.value['steps']['step$i']); 
+      }
+
+      loadedItems.add(Meal(
+        id: item.key,
+        categories: categories,
+        title: item.value['title'],
+        imageUrl: item.value['imageUrl'],
+        ingredients: ingredients,
+        steps: steps,
+      ));
+    }
+
+
+ 
+  }
+
+  void _selectCategory(BuildContext context, Category category) async {
+    final filteredMeals = loadedItems
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -20,6 +76,8 @@ class CategoriesScreen extends StatelessWidget {
         ),
       ),
     );
+
+    _loadItems();
   }
 
   @override
@@ -33,10 +91,10 @@ class CategoriesScreen extends StatelessWidget {
             const Text(
               "Wybierz kategorie...",
               textAlign: TextAlign.left,
-               style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
             Expanded(
               child: GridView(
